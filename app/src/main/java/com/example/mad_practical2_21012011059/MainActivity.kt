02126f84ab233f0b557d6df2ab629c2e78c144b3
vedant pansuriya <vedant15708@gmail.com>
2023-07-31 20:28:21 +0530

    pr

--- app/src/main/java/com/example/mad_practical2_21012011059/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical2_21012011059/MainActivity.kt
@@ -5,31 +5,46 @@ import android.os.Bundle
 import android.os.Message
 import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     val TAG="Mainactivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        showMessage("oncreate method is called");
+        showMessage("onCreate method is called");
     }
 
     override fun onStart() {
         super.onStart()
-        showMessage("onstart method is called")
+        showMessage("onStart method is called")
     }
 
     override fun onResume() {
         super.onResume()
-        showMessage("onresume method is called")
+        showMessage("onResume method is called")
     }
 
     override fun onRestart() {
         super.onRestart()
-        showMessage("onrestart method is called")
+        showMessage("onRestart method is called")
     }
 
-    
+    override fun onPause() {
+        super.onPause()
+        showMessage("onPause method is called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMessage("onStop method is called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showMessage("onDestroy method is called")
+    }
     fun showMessage(message:String) {
         Log.i(TAG,message)
         Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
